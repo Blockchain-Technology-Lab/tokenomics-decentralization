b7e54bea28806df20a831ed4500d5235048d9828
LadyChristina <christina_ovezik@outlook.com>
2023-04-03 17:04:26 +0100

    Save raw data to csv instead of json

--- collect_data.py
+++ collect_data.py
@@ -8,30 +8,45 @@
     root directory of the project under the name 'google-service-account-key.json'
 """
 import google.cloud.bigquery as bq
-import json
+import csv
 from yaml import safe_load
 import pathlib
+from time import time
 
 ROOT_DIR = pathlib.Path(__file__).resolve().parent
 INPUT_DIR = ROOT_DIR / 'input'
+
+if not INPUT_DIR.is_dir():
+    INPUT_DIR.mkdir()
 
 with open(ROOT_DIR / "queries.yaml") as f:
     queries = safe_load(f)
 
 client = bq.Client.from_service_account_json(json_credentials_path=ROOT_DIR / "google-service-account-key.json")
+
+force_query = False
 for ledger in queries.keys():
-    print(f"Querying {ledger}...")
+    filename = f'{ledger}_raw_data.csv'
+    file = INPUT_DIR / filename
+    if not force_query and file.is_file():
+        continue
+    print(f"Querying {ledger}..")
+    start = time()
     QUERY = (queries[ledger])
     query_job = client.query(QUERY)
     try:
         rows = query_job.result()
+        print(f'Done querying {ledger} (took about {round(time() - start)} seconds)')
     except Exception as e:
         print(f'{ledger} query failed, please make sure it is properly defined.')
         print(f'The following exception was raised: {repr(e)}')
         continue
 
-    result_json = [dict(row) for row in rows]
-
-    filename = f'{ledger}_raw_data.json'
-    with open(INPUT_DIR / filename, 'w') as f:
-        json.dump(result_json, f, default=str)
+    print(f"Writing {ledger} data to file..")
+    start = time()
+    with open(file, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow([field.name for field in rows.schema])
+        writer.writerows(rows)
+    print(f'Done writing {ledger} results (took about {round(time() - start)} seconds)')
+    print('-------------------------------------------------------')
