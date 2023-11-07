b59d17c4711a22b1589b18276a8a06bd316fb2e8
LadyChristina <christina_ovezik@outlook.com>
2023-11-07 18:22:26 +0000

    Add docstring to metric funcs

--- tokenomics_decentralization/metrics.py
+++ tokenomics_decentralization/metrics.py
@@ -2,6 +2,14 @@ from math import log
 
 
 def compute_tau(balance_distribution, threshold):
+    """
+    Calculates the tau index of a distribution of balances
+    :param balance_distribution: a list of integers, each corresponding to the balance of an entity
+    :param threshold: float, the parameter of the tau index, i.e. the threshold for the market share 
+        that is captured by the index
+    :returns: tuple of (int, float) where the first element is the tau index and the second is the 
+        percentage of the total market share that is captured by the tau index
+    """
     tau_index, tau_market_share = 0, 0
     circulation = sum(balance_distribution)
 
@@ -15,6 +23,11 @@ def compute_tau(balance_distribution, threshold):
 
 
 def compute_gini(balance_distribution):
+    """
+    Calculates the Gini coefficient of a distribution of balances
+    :param balance_distribution: a list of integers, each corresponding to the balance of an entity
+    :returns: float between 0 and 1 that represents the Gini coefficient of the given distribution
+    """
     parsed_entries = 0
     circulation = sum(balance_distribution)
     population = len(balance_distribution)
@@ -29,6 +42,11 @@ def compute_gini(balance_distribution):
 
 
 def compute_hhi(balance_distribution):
+    """
+    Calculates the Herfindahl-Hirschman index (HHI) of a distribution of balances
+    :param balance_distribution: a list of integers, each corresponding to the balance of an entity
+    :returns: float between 0 and 10,000 that represents the HHI of the given distribution
+    """
     circulation = sum(balance_distribution)
     hhi = 0
     for balance in balance_distribution:
@@ -39,6 +57,11 @@ def compute_hhi(balance_distribution):
 
 
 def compute_shannon_entropy(balance_distribution):
+    """
+    Calculates the Shannon entropy of a distribution of balances
+    :param balance_distribution: a list of integers, each corresponding to the balance of an entity
+    :returns: float between 0 and 1 that represents the Shannon entropy of the given distribution
+    """
     circulation = sum(balance_distribution)
     entropy = 0
     for balance in balance_distribution:
@@ -50,4 +73,9 @@ def compute_shannon_entropy(balance_distribution):
 
 
 def compute_total_entities(balance_distribution):
+    """
+    Calculates the total number of entities in a distribution of balances
+    :param balance_distribution: a list of integers, each corresponding to the balance of an entity
+    :returns: int that represents the total number of entities in the given distribution
+    """
     return len(balance_distribution)
