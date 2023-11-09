3180d5e8398da8e9b12b51cea578f298a4672305
Dimitris Karakostas <dimit.karakostas@gmail.com>
2023-11-09 08:54:22 +0000

    Use paths from helper in map.py

--- tokenomics_decentralization/map.py
+++ tokenomics_decentralization/map.py
@@ -1,16 +1,12 @@
 from tokenomics_decentralization.schema import get_connector
+from tokenomics_decentralization.helper import INPUT_DIR, MAPPING_INFO_DIR
 import os
 import sqlite3
 import json
 import csv
-import pathlib
 import logging
 
 logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
-
-ROOT_DIR = pathlib.Path(__file__).resolve().parent.parent
-RAW_DATA_PATH = ROOT_DIR / 'input'
-MAPPING_INFO_PATH = ROOT_DIR / 'mapping_information'
 
 
 def fill_db_with_addresses(conn, ledger):
@@ -28,7 +24,7 @@ def fill_db_with_addresses(conn, ledger):
     ledger_id = cursor.execute("SELECT id FROM ledgers WHERE name=?", (ledger, )).fetchone()[0]
 
     try:
-        with open(MAPPING_INFO_PATH / f'addresses/{ledger}.json') as f:
+        with open(MAPPING_INFO_DIR / f'addresses/{ledger}.json') as f:
             address_entities = json.load(f)
             for addr, info in address_entities.items():
                 entity = info['name']
@@ -63,7 +59,7 @@ def fill_db_with_balances(conn, ledger, snapshot):
 
     ledger_id = cursor.execute("SELECT id FROM ledgers WHERE name=?", (ledger, )).fetchone()[0]
 
-    input_file = RAW_DATA_PATH / f'{ledger}_{snapshot}_raw_data.csv'
+    input_file = INPUT_DIR / f'{ledger}_{snapshot}_raw_data.csv'
     if os.path.isfile(input_file):
         with open(input_file) as f:
             csv_reader = csv.reader(f, delimiter=',')
@@ -112,7 +108,7 @@ def fill_db_with_balances(conn, ledger, snapshot):
 
 def apply_mapping(ledger, snapshot, db_directories):
     logging.info(f'Mapping {ledger} {snapshot}')
-    input_filename = RAW_DATA_PATH / f'{ledger}_{snapshot}_raw_data.csv'
+    input_filename = INPUT_DIR / f'{ledger}_{snapshot}_raw_data.csv'
     db_paths = [db_dir / f'{ledger}_{snapshot}.db' for db_dir in db_directories]
     db_file = False
     for filename in db_paths:
