66699ba42293fea00a4cabb3a473d90d2d2fe5bc
Dimitris Karakostas <dimit.karakostas@gmail.com>
2023-11-28 10:22:09 +0000

    Test exclude_below_fees in get_output_row

--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -20,22 +20,28 @@ def test_get_output_row(mocker):
 
     metrics = {'hhi': 1, 'gini': 0}
     csv_row = get_output_row('bitcoin', '2010-01-01', metrics)
-    assert csv_row == ['bitcoin', '2010-01-01', False, False, 'absolute', 0, 1, 0]
+    assert csv_row == ['bitcoin', '2010-01-01', False, False, 'absolute', 0, False, 1, 0]
 
     get_no_clustering_mock.return_value = True
     metrics = {'non-clustered hhi': 1, 'non-clustered gini': 0}
     csv_row = get_output_row('bitcoin', '2010-01-01', metrics)
-    assert csv_row == ['bitcoin', '2010-01-01', True, False, 'absolute', 0, 1, 0]
+    assert csv_row == ['bitcoin', '2010-01-01', True, False, 'absolute', 0, False, 1, 0]
 
     get_exclude_contracts_mock.return_value = True
     metrics = {'exclude_contracts non-clustered hhi': 1, 'exclude_contracts non-clustered gini': 0}
     csv_row = get_output_row('bitcoin', '2010-01-01', metrics)
-    assert csv_row == ['bitcoin', '2010-01-01', True, True, 'absolute', 0, 1, 0]
+    assert csv_row == ['bitcoin', '2010-01-01', True, True, 'absolute', 0, False, 1, 0]
 
     get_top_limit_value_mock.return_value = 1
     metrics = {'top-1_absolute exclude_contracts non-clustered hhi': 1, 'top-1_absolute exclude_contracts non-clustered gini': 0}
     csv_row = get_output_row('bitcoin', '2010-01-01', metrics)
-    assert csv_row == ['bitcoin', '2010-01-01', True, True, 'absolute', 1, 1, 0]
+    assert csv_row == ['bitcoin', '2010-01-01', True, True, 'absolute', 1, False, 1, 0]
+
+    get_exclude_below_fees_mock.return_value = True
+    get_top_limit_value_mock.return_value = 1
+    metrics = {'top-1_absolute exclude_below_fees exclude_contracts non-clustered hhi': 1, 'top-1_absolute exclude_below_fees exclude_contracts non-clustered gini': 0}
+    csv_row = get_output_row('bitcoin', '2010-01-01', metrics)
+    assert csv_row == ['bitcoin', '2010-01-01', True, True, 'absolute', 1, True, 1, 0]
 
 
 def test_analyze_snapshot(mocker):
