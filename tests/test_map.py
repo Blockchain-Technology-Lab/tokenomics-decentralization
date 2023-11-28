4c670a95e31570dc7099c786592ec91890637a9f
Dimitris Karakostas <dimit.karakostas@gmail.com>
2023-11-28 15:23:35 +0000

    Test apply_mapping

--- tests/test_map.py
+++ tests/test_map.py
@@ -1,5 +1,6 @@
-from tokenomics_decentralization.map import fill_db_with_addresses
+from tokenomics_decentralization.map import fill_db_with_addresses, apply_mapping
 from unittest.mock import call
+import pathlib
 
 
 def test_fill_db_with_addresses(mocker):
@@ -32,3 +33,68 @@ def test_fill_db_with_addresses(mocker):
     assert call('connector', 'bitcoin', 'address 1', 'Entity Name 1', False) in db_insert_update_address_mock.call_args_list
     assert call('connector', 'bitcoin', 'address 2', 'Entity Name 2', True) in db_insert_update_address_mock.call_args_list
     db_commit_mock.assert_called_with()
+
+
+def test_apply_mapping(mocker):
+    get_db_connector_mock = mocker.patch('tokenomics_decentralization.map.get_connector')
+    get_db_connector_mock.return_value = 'connector'
+
+    fill_db_with_addresses_mock = mocker.patch('tokenomics_decentralization.map.fill_db_with_addresses')
+    fill_db_with_balances_mock = mocker.patch('tokenomics_decentralization.map.fill_db_with_balances')
+
+    get_force_map_addresses_mock = mocker.patch('tokenomics_decentralization.helper.get_force_map_addresses_flag')
+    get_force_map_balances_mock = mocker.patch('tokenomics_decentralization.helper.get_force_map_balances_flag')
+
+    get_output_directories_mock = mocker.patch('tokenomics_decentralization.helper.get_output_directories')
+    get_input_directories_mock = mocker.patch('tokenomics_decentralization.helper.get_input_directories')
+
+    os_isfile_mock = mocker.patch('os.path.isfile')
+    os_isfile_mock.side_effect = {
+        pathlib.Path('/output').resolve() / 'bitcoin_2010-01-01.db': False,
+        pathlib.Path('/input1').resolve() / 'bitcoin_2010-01-01_raw_data.csv': False,
+        pathlib.Path('/input2').resolve() / 'bitcoin_2010-01-01_raw_data.csv': False,
+    }.get
+
+    get_force_map_addresses_mock.return_value = False
+    get_force_map_balances_mock.return_value = False
+
+    get_output_directories_mock.return_value = [pathlib.Path('/output').resolve()]
+    get_input_directories_mock.return_value = [pathlib.Path('/input1').resolve(), pathlib.Path('/input2').resolve()]
+
+    apply_mapping('bitcoin', '2010-01-01')
+    assert get_db_connector_mock.call_args_list == []
+    assert fill_db_with_addresses_mock.call_args_list == []
+    assert fill_db_with_balances_mock.call_args_list == []
+
+    os_isfile_mock.side_effect = {
+        pathlib.Path('/output').resolve() / 'bitcoin_2010-01-01.db': True,
+    }.get
+    get_input_directories_mock.reset_mock()
+
+    apply_mapping('bitcoin', '2010-01-01')
+    assert get_input_directories_mock.call_args_list == []
+    assert fill_db_with_addresses_mock.call_args_list == []
+    assert fill_db_with_balances_mock.call_args_list == []
+
+    get_force_map_addresses_mock.return_value = True
+    get_force_map_balances_mock.return_value = True
+
+    apply_mapping('bitcoin', '2010-01-01')
+    assert fill_db_with_addresses_mock.call_args_list == [call('connector', 'bitcoin')]
+    assert fill_db_with_balances_mock.call_args_list == [call('connector', 'bitcoin', '2010-01-01')]
+
+    os_isfile_mock.side_effect = {
+        pathlib.Path('/output').resolve() / 'bitcoin_2010-01-01.db': False,
+        pathlib.Path('/input1').resolve() / 'bitcoin_2010-01-01_raw_data.csv': True
+    }.get
+
+    get_force_map_addresses_mock.return_value = False
+    get_force_map_balances_mock.return_value = False
+    get_input_directories_mock.reset_mock()
+    fill_db_with_addresses_mock.reset_mock()
+    fill_db_with_balances_mock.reset_mock()
+
+    apply_mapping('bitcoin', '2010-01-01')
+    assert get_input_directories_mock.call_args_list == [call()]
+    assert fill_db_with_addresses_mock.call_args_list == [call('connector', 'bitcoin')]
+    assert fill_db_with_balances_mock.call_args_list == [call('connector', 'bitcoin', '2010-01-01')]
